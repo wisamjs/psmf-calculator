113c2a60880805c5222e43c9b683518b19d8f8e8
Wisam Zaghal <wisamzaghal@gmail.com>
2016-10-19 16:51:53 -0400

    initial dump to react container

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -5,12 +5,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+        <div className="description">
+          <h2 className="lato">How much do you weigh?</h2>
+          <p className="open-sans greyed-out">This will help us calculate your lean mass and total calorie intake</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="forms">
+          <form
+          name="calculator"
+          id="calculator"
+          className="lato">
+            <div className="field">
+              <input
+              name="weight"
+              type="number"
+              pattern="[0-9]*"
+              placeholder="150"/>
+              <span className="lato greyed-out fs21">lbs</span>
+              <p className="greyed-out lato fs12"> 82 Kg</p>
+            </div>
+          </form>
+        </div>
+
+        <button>Continue</button>
       </div>
     );
   }
