6ea54ffcfee73a9ccc241559a0fe18a0fa0489b4
Wisam Zaghal <wisamzaghal@gmail.com>
2016-10-19 17:41:39 -0400

    more css and html cleanup

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,30 +4,30 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="mx2 flex justify-between flex-column">
         <div className="description">
-          <h2 className="lato">How much do you weigh?</h2>
-          <p className="open-sans greyed-out">This will help us calculate your lean mass and total calorie intake</p>
+          <h2 className="lato center">How much do you weigh?</h2>
+          <p className="open-sans center greyed-out">This will help us calculate your lean mass and total calorie intake</p>
         </div>
 
-        <div className="forms">
+        <div className="mb4">
           <form
           name="calculator"
-          id="calculator"
-          className="lato">
-            <div className="field">
+          className="lato center py2">
+            <div>
               <input
               name="weight"
+              className="h2 my2 px0 py1"
               type="number"
               pattern="[0-9]*"
               placeholder="150"/>
-              <span className="lato greyed-out fs21">lbs</span>
-              <p className="greyed-out lato fs12"> 82 Kg</p>
+              <span className="lato grey h3">lbs</span>
+              <p className="grey lato h6"> 82 Kg</p>
             </div>
           </form>
         </div>
 
-        <button>Continue</button>
+        <button className="uppercase my3 px3 py2 h6 bg-blue white">Continue</button>
       </div>
     );
   }
