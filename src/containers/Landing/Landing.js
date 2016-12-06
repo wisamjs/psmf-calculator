6ba456f953d7eaa2e91ff39225c6dd94bd153408
wzaghal <wisamzaghal@gmail.com>
2016-12-06 10:27:42 -0500

    add Scale to landing page

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -3,15 +3,19 @@ import React from 'react';
 import Description from '../../components/Description';
 import Title from '../../components/Title';
 import Button from '../../components/Button';
+import Scale from '../../components/Scale';
 
 const Landing = () => {
   return (
-    <div className="mx2 flex justify-between flex-column">
+    <div className="mx3 flex flex-column">
       <div>
         <Title>PSMF Calculator</Title>
         <Description>
           This will help us calculate your lean mass and total calorie intake
         </Description>
+      </div>
+      <div className="mx3 my2">
+        <Scale/>
       </div>
 
       <Button path="weight"/>
